0d4e6315eed395e1739f969b382a1d92e083fd87
Bobby Neal <rsneal01@gmail.com>
2020-05-21 17:01:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,18 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genres = []
   
   def initialize
     @@count += 1
+    @@genres << :genre
   end
   
   def self.count
     @@count
+  end
+  
+  def self.genres
+    
+  end
 end
