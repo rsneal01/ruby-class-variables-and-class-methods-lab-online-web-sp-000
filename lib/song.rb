35eb558b0839ae0ae43bd0328162e2c17e64332f
Bobby Neal <rsneal01@gmail.com>
2020-05-21 18:30:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
   def self.genre_count
     genre_count_hash = {}
     @@genres.each do |genre|
-      genre_count_hash[genre] 
+      genre_count_hash[genre] = 
       
       # binding.pry
     end
