13f063c5edd631d67e22618b9781f3395073604d
Bobby Neal <rsneal01@gmail.com>
2020-05-21 17:11:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,8 @@ class Song
   
   def initialize(name, artist, genre)
     @name = name
+    @artist = artist
+    @genre = genre
     @@count += 1
     @@genres << @genre
     @@artists << @artist
